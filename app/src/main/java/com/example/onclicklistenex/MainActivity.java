9974a9aab177b543e24b4aee21ee9de67ac9799d
Amol Wahane <57882977+amowahane17@users.noreply.github.com>
2022-06-19 21:23:42 +0530

    FINISHED NEED TO ADD COMMENTS FOR BETTER UNDERSTANDING!

--- app/src/main/java/com/example/onclicklistenex/MainActivity.java
+++ app/src/main/java/com/example/onclicklistenex/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.onclicklistenex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textView_1, firstWay, second, secondWay, third, thirdWay;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView_1 = findViewById(R.id.first_btn);
+        firstWay = findViewById(R.id.firstWay_btn);
+
+        second = findViewById(R.id.second_btn);
+        secondWay = findViewById(R.id.secondWay_btn);
+
+        third = findViewById(R.id.third_btn);
+        thirdWay = findViewById(R.id.thirdWay_btn);
+
+        second.setOnClickListener(this);
+        secondWay.setOnClickListener(this);
+
+
+         textView_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Android app Dev", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        firstWay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "By AMOL", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.second_btn:
+                Toast.makeText(this, "Amol", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.secondWay_btn:
+                Toast.makeText(this, "Wahane", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    public void meth_third(View view){
+        Toast.makeText(this, "amol", Toast.LENGTH_SHORT).show();
+    }
+
+    public void meth_way_third(View view){
+        Toast.makeText(this, "wahane", Toast.LENGTH_SHORT).show();
+    }
+
 }
